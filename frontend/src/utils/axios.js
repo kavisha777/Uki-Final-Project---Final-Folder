4196ce924b22c3213ab26f00b670bb915e0a6228
kavisha777 <kaviyohen@gmail.com>
2025-08-19 13:35:43 +0530

    change axios file

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // change port if different
+  // baseURL: 'http://localhost:5000/api', // change port if different
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, // if you're using cookies (for auth)
 });
 
